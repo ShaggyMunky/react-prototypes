00e696d97505911a53fb712aa6625266f6c6c672
Aaron Guillen <aaronguillen21@gmail.com>
2018-01-24 14:30:25 -0800

    first implementation of functional componenets

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,31 @@
+import React from "react";
+import Table from "./table";
 
 
+const App = () => {
+    const students = [
+        {
+            name: "First Name",
+            course: "This Course",
+            grade: 89
+        },
+        {
+            name: "Second Name",
+            course: "That Course",
+            grade: 90
+        },
+        {
+            name: "Third Name",
+            course: "Another One",
+            grade: 70
+        }
+    ];
+    return (
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+};
+
+export default App;

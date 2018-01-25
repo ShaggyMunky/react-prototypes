ef6cf4eef4d3891b475f1ab71b3a78b478a66547
Aaron Guillen <aaronguillen21@gmail.com>
2018-01-25 00:06:20 -0800

    created stopwatch and added bootstrap styling. Working on keeping text always centered

---
+++ src/components/format_time.js
@@ -0,0 +1,50 @@
+import React, {Component} from "react";
+
+class FormatTime extends Component{
+    constructor(props){
+        super (props);
+
+        this.state = {
+            hour: 0,
+            min: 0,
+            sec: 0,
+            ms: 0
+        };
+        this.timerStyle = {
+            display: "inline-block",
+            position: "relative",
+            left: "50%",
+            transform: "translate(-50%)",
+            width: "95%",
+            fontSize: "11vmin",
+        }
+    };
+    componentWillReceiveProps(nextProps){
+        let {elapsed} = nextProps;
+        let sec = elapsed / 1000;
+        let min = sec / 60;
+
+        this.setState({
+            hour: min / 60 >> 0,
+            min: min % 60 >> 0,
+            sec: sec % 60 >> 0,
+            ms: elapsed % 100 >> 0
+        })
+    };
+    leadingZero(number){
+       return number < 10 ? `0${number}` : number;
+    };
+    trailingZero(number){
+      return number < 10 ? `${number}0` : number;
+    };
+    render(){
+        const {hour, min, sec, ms} = this.state;
+        return(
+          <div style={this.timerStyle}>
+              {this.leadingZero(hour)}:{this.leadingZero(min)}:{this.leadingZero(sec)}.{this.trailingZero(ms)}
+          </div>
+        );
+    };
+}
+
+export default FormatTime;

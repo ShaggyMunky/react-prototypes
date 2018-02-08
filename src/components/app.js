d1c8e4060fc81f70478962b32b2d68c4faf64bb6
ShaggyMunky <aaronguillen21@gmail.com>
2018-02-08 15:24:31 -0800

    completed redux prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,14 @@
+import React from "react";
+import Clock from "./clock";
 
+const App = () =>{
+    return(
+        <div className="container">
+            <h1 className="text-center">Any Text I want</h1>
+            <Clock/>
+        </div>
+    )
+};
+
+export default App;
 

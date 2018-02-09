b5bbc95b2ac33c0588d94d8f5718f2c9f678e70e
ShaggyMunky <aaronguillen21@gmail.com>
2018-02-08 16:11:09 -0800

    completed basic featuresets

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 const Movie = props => {
+    const {info} = props;
     return(
         <div>
-            <h3>Movie</h3>
+            <img src={info["im:image"][2].label}/>
+            <h3>{info["im:name"].label}</h3>
+            <p>{info.summary.label}</p>
         </div>
     );
 };
+
+export default Movie;
---
+++ src/components/movies_container.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+import axios from "axios";
+import Movie from "./movie";
+
+class MoviesContainer extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            movies: []
+        }
+    }
+    componentWillMount(){
+        const ITUNES_URL = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json";
+
+        axios.get(ITUNES_URL).then((resp) =>{
+            this.setState({
+                movies: resp.data.feed.entry
+            })
+        });
+    }
+    render(){
+        const movieList = this.state.movies.map((movieInfo, index) =>{
+            return(
+                <Movie info={movieInfo} key={index}/>
+            );
+        });
+        return(
+            <div>
+                {movieList}
+            </div>
+        );
+    }
+}
+
+export default MoviesContainer;
